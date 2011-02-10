f56391339f66b9210afb49b30a37924940aefb9b
Stefan Kanev <stefan.kanev@gmail.com>
2011-02-11 00:39:49 +0100

    Properly sporkified the application

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,13 @@ module Trane
     config.active_record.schema_format = :sql
 
     config.i18n.default_locale = :bg
+
+    ### Part of a Spork hack. See http://bit.ly/arY19y
+    if Rails.env.test?
+      initializer :after => :initialize_dependency_mechanism do
+        # Work around initializer in railties/lib/rails/application/bootstrap.rb
+        ActiveSupport::Dependencies.mechanism = :load
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,20 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require 'rubygems'
+require 'spork'
 
-require 'factory_girl/syntax/make'
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'factory_girl/syntax/make'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+  ActiveSupport::Dependencies.clear
+end
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.use_transactional_fixtures = true
+Spork.each_run do
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = true
+  end
 end
