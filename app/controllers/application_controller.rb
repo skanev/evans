8b51701980e9636cd9a13fa589aabd03a3847b56
Stefan Kanev <stefan.kanev@gmail.com>
2011-02-20 00:45:14 +0100

    Added require_user before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def require_user
+    unless current_user
+      redirect_to root_path, :flash => {:error => 'Трябва да влезнете в системата за да направите това.'}
+    end
+  end
+
   def require_admin
     unless current_user.try(:admin?)
-      redirect_to root_path, :flash => {:error => 'Нямате достъп до тази страница'}
+      redirect_to root_path, :flash => {:error => 'Нямате достъп до тази страница.'}
     end
   end
 end
