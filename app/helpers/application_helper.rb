35fa764e696f1245852425a439f2ae33d62a51fc
Stefan Kanev <stefan.kanev@gmail.com>
2011-02-23 00:33:56 +0100

    A helper for displaying user photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_thumbnail(user)
+    image = user.photo.try(:url, :thumb) || image_path('photoless-user.png')
+    image_tag image, :alt => user.full_name
+  end
 end
