382d08fbfbea7db948bf016767bf38e253f0e91e
Stefan Kanev <stefan.kanev@gmail.com>
2011-03-03 17:59:51 +0200

    Use attr_protected instead of attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :rememberable, :trackable
 
-  attr_accessible :password, :password_confirmation, :remember_me, :photo, :remove_photo
-  attr_accessible :github, :twitter, :skype, :phone, :site, :about
+  attr_protected :full_name, :faculty_number, :email, :admin
 
   mount_uploader :photo, PhotoUploader
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,8 @@
 require 'spec_helper'
 
 describe User do
+  it { should_not allow_mass_assignment_of(:faculty_number) }
+  it { should_not allow_mass_assignment_of(:full_name) }
+  it { should_not allow_mass_assignment_of(:email) }
+  it { should_not allow_mass_assignment_of(:admin) }
 end
