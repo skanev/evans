75cd87dea5c96191ec81bd26c2981c22e183c034
Stefan Kanev <stefan.kanev@gmail.com>
2011-03-06 17:38:32 +0200

    Added Announcement#page

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,4 +1,12 @@
 class Announcement < ActiveRecord::Base
+  self.per_page = 10
+
   validates_presence_of :title
   validates_presence_of :body
+
+  class << self
+    def page(number)
+      order('created_at DESC').paginate :page => number, :per_page => per_page
+    end
+  end
 end
--- spec/models/announcement_spec.rb
+++ spec/models/announcement_spec.rb
@@ -3,4 +3,14 @@ require 'spec_helper'
 describe Announcement do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
+
+  it "paginates announcements in reverse chronological order" do
+    second = Announcement.make :created_at => 2.days.ago
+    first  = Announcement.make :created_at => 1.day.ago
+
+    Announcement.stub :per_page => 1
+
+    Announcement.page(1).should == [first]
+    Announcement.page(2).should == [second]
+  end
 end
