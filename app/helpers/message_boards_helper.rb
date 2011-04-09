53b79c9c4ffb795d9b3402580bd351593c13a299
Andrew Radev <andrey.radev@gmail.com>
2011-04-09 17:21:36 +0300

    Extract starring link to a helper

--- app/helpers/message_boards_helper.rb
+++ app/helpers/message_boards_helper.rb
@@ -6,4 +6,12 @@ module MessageBoardsHelper
       else raise "Argument is not a post: #{post.class}"
     end
   end
+
+  def toggle_post_star_link(post)
+    if post.starred?
+      link_to 'Махни звездичката', post_star_path(post), :method => :delete
+    else
+      link_to 'Дай звездичка', post_star_path(post), :method => :post
+    end
+  end
 end
---
+++ spec/views/topics/_post.html.haml_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'topics/_post.html.haml' do
+  let(:post) { Factory.stub(:topic, :created_at => Time.now) }
+
+  before do
+    view.stub :post => post
+    view.stub :can_edit?
+    view.stub :admin?
+  end
+
+  it "renders a link to admins for toggling the post star" do
+    view.stub :admin? => true
+    view.stub :toggle_post_star_link => 'toggle star link'
+
+    render
+
+    rendered.should contain('toggle star link')
+  end
+
+  it "renders successfully" do
+    render
+  end
+end
