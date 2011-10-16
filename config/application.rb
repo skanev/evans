cee45d4ec893dfe572c777061ea0f1cb04d094ba
Dimitar Dimitrov <wireman@gmail.com>
2011-10-16 17:57:41 +0200

    Fix the missing lib/foreign_keys on production

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Trane
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %w(lib/)
+    config.autoload_paths += ["#{config.root}/lib"]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
