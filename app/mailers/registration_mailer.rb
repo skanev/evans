8344b2f1c6c5e130cd9fbd07de30e7b4a4a2d0a2
Stefan Kanev <stefan.kanev@gmail.com>
2011-10-17 00:47:12 +0300

    An experiment with the mailer

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class RegistrationMailer < ActionMailer::Base
-  default :from => 'evans@ruby.bg', :reply_to => 'fmi@ruby.bg'
+  default :from => '"Ruby ФМИ" <evans@ruby.bg>', :reply_to => '"Ruby ФМИ" <fmi@ruby.bg>'
 
   def confirmation(sign_up)
     @activation_url = activation_url(sign_up.token, :only_path => false)
