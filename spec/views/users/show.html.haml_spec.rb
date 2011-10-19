5e52912f1ede424db0be190466153a0642e0fd81
Dimitar Dimitrov <wireman@gmail.com>
2011-10-19 11:10:48 +0200

    Link to to edit your profile from your own public profile page
    Some context rearrangements in the users/show.html spec were needed

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -6,6 +6,7 @@ describe "users/show.html.haml" do
 
   before do
     view.stub :admin? => false
+    view.stub :edit_profile_path => '/profile/edit'
 
     points_breakdown.stub :each_starred_post_with_title
     points_breakdown.stub :having_starred_posts?
@@ -14,13 +15,45 @@ describe "users/show.html.haml" do
     assign :points_breakdown, points_breakdown
   end
 
-  it "shows a links to starred posts of the user" do
-    points_breakdown.stub :having_starred_posts? => true
-    points_breakdown.should_receive(:each_starred_post_with_title).and_yield(Factory.stub(:reply), "Post title")
-    view.stub :post_path => '/post-path'
+  context 'when a user is not logged in' do
+    before do
+      view.stub :logged_in? => false
+      view.stub :current_user => nil
+    end
 
-    render
+    it "shows links to starred posts of the user" do
+      points_breakdown.stub :having_starred_posts? => true
+      points_breakdown.should_receive(:each_starred_post_with_title).and_yield(Factory.stub(:reply), "Post title")
+      view.stub :post_path => '/post-path'
 
-    rendered.should have_selector("a[href='/post-path']:contains('Post title')")
+      render
+
+      rendered.should have_selector("a[href='/post-path']:contains('Post title')")
+    end
+
+    it "doesn't show a link to the edit profile page" do
+      render
+      rendered.should_not have_selector("a[href='/profile/edit']")
+    end
+  end
+
+  context 'when a user is logged in' do
+    before do
+      view.stub :logged_in? => true
+      view.stub :current_user => user
+    end
+
+    it "shows a link to the edit profile page if viewing user's own profile" do
+      render
+      rendered.should have_selector("a[href='/profile/edit']")
+    end
+
+    it "doesn't show a link to the edit profile page if viewing someone else's profile" do
+      assign :user, Factory.stub(:user)
+
+      render
+
+      rendered.should_not have_selector("a[href='/profile/edit']")
+    end
   end
 end
