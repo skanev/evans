263bbbf056615d0675eb1a1aeeeb182161cbb1c6
Stefan Kanev <stefan.kanev@gmail.com>
2011-10-20 18:07:54 +0300

    Remove a bunch of blank lines

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -16,43 +16,36 @@ describe CommentsController do
 
     it "looks up the solution by id" do
       Solution.should_receive(:find).with('42')
-
       post :create, task_id: '1', solution_id: '42'
     end
 
     it "assigns the solution to @solution" do
       post :create, task_id: '1', solution_id: '42'
-
       controller.should assign_to(:solution).with(solution)
     end
 
     it "assigns the comment to @comment" do
       post :create, task_id: '1', solution_id: '42'
-
       controller.should assign_to(:comment).with(comment)
     end
 
     it "creates a new comment with the given attributes" do
       solution.comments.should_receive(:build).with('comment-attributes')
-
       post :create, task_id: '1', solution_id: '2', comment: 'comment-attributes'
     end
 
     it "creates a new comment on the solution on behalf of the user" do
       comment.should_receive(:user=).with(current_user)
-
       post :create, task_id: '1', solution_id: '2'
     end
 
     it "attempts to save the comment" do
       comment.should_receive :save
-
       post :create, task_id: '1', solution_id: '2'
     end
 
     it "redirects to the solution on success" do
       comment.stub save: true
-
       post :create, task_id: '1', solution_id: '2'
 
       controller.should redirect_to(solution)
@@ -60,9 +53,7 @@ describe CommentsController do
 
     it "redisplays the comment for editing on failure" do
       comment.stub save: false
-
       post :create, task_id: '1', solution_id: '2'
-
       controller.should render_template(:new)
     end
   end
