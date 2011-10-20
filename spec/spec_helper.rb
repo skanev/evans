ce9435187bf09b89c5a9191a8ea078a1d82997fd
Stefan Kanev <stefan.kanev@gmail.com>
2011-10-20 20:31:01 +0300

    Add CustomPaths to mailer specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,5 +28,6 @@ Spork.each_run do
     config.include Support::ControllerHelpers, :type => :controller
     config.include EmailSpec::Helpers, type: :mailer
     config.include EmailSpec::Matchers, type: :mailer
+    config.include CustomPaths, type: :mailer
   end
 end
