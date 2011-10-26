4065801ebb2c34695b7666dbb907e7cc85aa39db
Stefan Kanev <stefan.kanev@gmail.com>
2011-10-26 20:37:52 +0300

    Convert a spec to Ruby 1.9

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Submission do
-  let(:user) { FactoryGirl.create :user }
-  let(:task) { FactoryGirl.create :task }
+  let(:user) { create :user }
+  let(:task) { create :task }
 
   it "creates a new solution for the given user and task" do
     submit user, task, 'code'
 
-    Solution.exists?(:user_id => user.id, :task_id => task.id, :code => 'code').should be_true
+    Solution.exists?(user_id: user.id, task_id: task.id, code: 'code').should be_true
   end
 
   it "updates the current solution if it exists" do
-    solution = FactoryGirl.create :solution, :user => user, :task => task, :code => 'old code'
+    solution = create :solution, user: user, task: task, code: 'old code'
 
     submit user, task, 'new code'
 
@@ -28,8 +28,8 @@ describe Submission do
   end
 
   it "does not update the solution after the task is closed" do
-    task = Factory(:closed_task)
-    solution = Factory(:solution, :task => task, :user => user, :code => 'old code')
+    task = create(:closed_task)
+    solution = create(:solution, task: task, user: user, code: 'old code')
 
     submit user, task, 'new code'
 
