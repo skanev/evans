fc52c3d918b22094717d17cba3617262119552ff
Stefan Kanev <stefan.kanev@gmail.com>
2011-11-01 22:05:43 +0200

    Style display of points in solutions

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,12 +1,26 @@
 $(function() {
   $('td:has([data-points])').each(function() {
-    var td = $(this);
-    var points = parseInt($('[data-points]', td).text(), 10);
+    var td         = $(this),
+        points     = parseInt($('[data-points]', td).text(), 10),
+        adjustment = parseInt($('[data-adjustment]', td).text(), 10);
 
     td.html('');
 
-    for(var i = 0; i < points; i++) {
-      $('<span class="point"></span>').appendTo(td);
+
+    if (adjustment >= 0) {
+      for(var i = 0; i < points - adjustment; i++) {
+        $('<span class="point"></span>').appendTo(td);
+      }
+      for(var i = 0; i < adjustment; i++) {
+        $('<span class="bonus-point"></span>').appendTo(td);
+      }
+    } else if (adjustment < 0) {
+      for(var i = 0; i < points; i++) {
+        $('<span class="point"></span>').appendTo(td);
+      }
+      for(var i = 0; i < -adjustment; i++) {
+        $('<span class="penalty-point"></span>').appendTo(td);
+      }
     }
   });
 
