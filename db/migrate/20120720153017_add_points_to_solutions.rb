c897135ba48a978ce9573bd88822fde1400e25e6
Stefan Kanev <stefan.kanev@gmail.com>
2012-07-21 21:56:30 +0300

    Update a migration
    Adding points to solutions does not calculate proper points. This fixes
    it (although there are rounding errors in 2 cases).
    
    Updating a migration is evil, let's overlook that in this case, since
    nobody has deployed anyway.

--- db/migrate/20120720153017_add_points_to_solutions.rb
+++ db/migrate/20120720153017_add_points_to_solutions.rb
@@ -1,5 +1,15 @@
 class AddPointsToSolutions < ActiveRecord::Migration
   def change
     add_column :solutions, :points, :integer, null: false, default: 0
+    execute <<-SQL
+      UPDATE solutions
+      SET points = GREATEST(0,
+        CASE WHEN (failed_tests + passed_tests = 0)
+          THEN 0
+          ELSE round(passed_tests::numeric / (failed_tests::numeric + passed_tests::numeric) * tasks.max_points)
+        END)
+      FROM tasks
+      WHERE solutions.task_id = tasks.id
+    SQL
   end
 end
