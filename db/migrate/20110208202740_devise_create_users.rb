67b62e6bd1abccffc3a4fddd8ab96b65cf43dd27
Dimitar Dimitrov <me@ddimitrov.name>
2012-10-03 22:06:08 +0300

    Update an old migration to work with Devise 2.x

--- db/migrate/20110208202740_devise_create_users.rb
+++ db/migrate/20110208202740_devise_create_users.rb
@@ -4,10 +4,23 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string    :faculty_number, :null => false
       t.string    :full_name,      :null => false
 
-      t.database_authenticatable   :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      # Database authenticable
+      t.string    :email,              :null => false, :default => ''
+      t.string    :encrypted_password, :null => false, :default => ''
+
+      # Recoverable
+      t.string    :reset_password_token
+      t.datetime  :reset_password_sent_at
+
+      # Rememberable
+      t.datetime  :remember_created_at
+
+      # Trackable
+      t.integer   :sign_in_count, :default => 0
+      t.datetime  :current_sign_in_at
+      t.datetime  :last_sign_in_at
+      t.string    :current_sign_in_ip
+      t.string    :last_sign_in_ip
 
       t.timestamps
     end
