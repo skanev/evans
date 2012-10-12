1e46953330d044c96e482a65ff8b98b0c8b78854
ignisf <bordjukov@gmail.com>
2012-10-12 19:17:10 +0300

    Refactored the specs of the users/show view
    See https://github.com/skanev/evans/pull/42#discussion_r1829558

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -2,71 +2,95 @@ require 'spec_helper'
 
 describe "users/show.html.haml" do
   let(:user) { build_stubbed :user }
+  let(:have_edit_link) { have_selector("a[href='#{edit_profile_path}']") }
 
   before do
     view.stub admin?: false
-    view.stub edit_profile_path: '/profile/edit'
-
     assign :user, user
   end
 
-  context 'when a user is not logged in' do
+  context 'when a visitor is viewing a user profile' do
     before do
       view.stub logged_in?: false
       view.stub current_user: nil
     end
 
-    it "doesn't show a link to the edit profile page" do
+    it "does not show a link to the edit profile page" do
       render
-      rendered.should_not have_selector("a[href='/profile/edit']")
+      rendered.should_not have_edit_link
     end
 
-    it "doesn't show the faculty number" do
+    it "does not show the faculty number" do
       render
       rendered.should_not have_content(user.faculty_number)
     end
+
+    it "does not show the email" do
+      render
+      rendered.should_not have_content(user.email)
+    end
   end
 
-  context 'when a user is logged in' do
+  context 'when a user is viewing their own profile' do
     before do
       view.stub logged_in?: true
       view.stub current_user: user
     end
 
-    it "shows a link to the edit profile page if viewing user's own profile" do
+    it "shows a link to the edit profile page" do
       render
-      rendered.should have_selector("a[href='/profile/edit']")
+      rendered.should have_edit_link
     end
 
-    it "doesn't show a link to the edit profile page if viewing someone else's profile" do
-      assign :user, build_stubbed(:user)
-
+    it "shows their faculty number" do
       render
-
-      rendered.should_not have_selector("a[href='/profile/edit']")
+      rendered.should have_content(user.faculty_number)
     end
 
-    describe "faculty number" do
-      it "is visible if viewing user's own profile" do
-        render
-        rendered.should have_selector(".faculty-number")
-      end
-      
-      it "is visible if viewing someone else's profile as admin" do
-        view.stub admin?: true
-        assign :user, build_stubbed(:user)
+    it "does not show the email" do
+      render
+      rendered.should_not have_content(user.email)
+    end
+  end
 
-        render
-        rendered.should have_selector(".faculty-number")
-      end
+  context "when a user is viewing somebody else's profile" do
+    before do
+      view.stub logged_in?: true
+      view.stub current_user: build_stubbed(:user)
+    end
 
-      it "is not visible if viewing someone else's profile" do
-        other_user = build_stubbed(:user)
-        assign :user, other_user
+    it "does not show a link to the edit profile page" do
+      render
+      rendered.should_not have_edit_link
+    end
 
-        render
-        rendered.should_not have_content(other_user.faculty_number)
-      end
+    it "does not show the faculty number" do
+      render
+      rendered.should_not have_content(user.faculty_number)
+    end
+
+    it "does not show the email" do
+      render
+      rendered.should_not have_content(user.email)
+    end
+  end
+
+  context 'when an admin is viewing a profile' do
+    before do
+      view.stub logged_in?: true
+      view.stub admin?: true
+      view.stub current_user: build_stubbed(:admin)
+      render
+    end
+
+    it "shows the faculty number" do
+      render
+      rendered.should have_content(user.faculty_number)
+    end
+
+    it "shows the email" do
+      render
+      rendered.should have_content(user.email)
     end
   end
 end
