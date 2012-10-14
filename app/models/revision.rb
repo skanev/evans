256cede6e4e0c4d47a9f5e8da03d7163149d776f
Stefan Kanev <stefan.kanev@gmail.com>
2012-10-15 00:19:08 +0300

    Revision comments should be sorted chronologically

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -2,7 +2,7 @@ class Revision < ActiveRecord::Base
   validates :code, presence: true
 
   belongs_to :solution
-  has_many :comments
+  has_many :comments, order: 'comments.created_at ASC'
 
   delegate :task, :commentable_by?, to: :solution
 end
--- spec/models/revision_spec.rb
+++ spec/models/revision_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Revision do
   it { should validate_presence_of :code }
+
+  it "orders the comments chronologically" do
+    revision = create :revision
+
+    second = create :comment, revision: revision, created_at: 1.day.ago
+    first  = create :comment, revision: revision, created_at: 2.days.ago
+
+    revision.comments.should eq [first, second]
+  end
 end
