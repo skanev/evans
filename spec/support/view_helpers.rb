8090de841725ebfccfe2194751acdfd6e49bdee5
Stefan Kanev <stefan.kanev@gmail.com>
2012-10-24 22:06:14 +0300

    Fix the have_link_to matcher
    Either you're providing a helper function that returns a matcher or
    implementing a matcher separately. You should not define a a matcher
    that uses another matcher.

--- spec/support/view_helpers.rb
+++ spec/support/view_helpers.rb
@@ -1,9 +1,7 @@
 module Support
   module ViewHelpers
-    RSpec::Matchers.define :have_link_to do |url|
-      match do |view|
-        view.should have_selector "a[href='#{url}']"
-      end
+    def have_link_to(url)
+      have_selector "a[href='#{url}']"
     end
   end
 end
