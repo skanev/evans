b191bad2b4a14e00eda135b642282d0f2b6df5d3
Stefan Kanev <stefan.kanev@gmail.com>
2012-11-08 17:04:39 +0200

    Extract an input field for DateTime

---
+++ app/inputs/date_time_input.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+class DateTimeInput < SimpleForm::Inputs::Base
+  FORMAT = '%Y-%m-%d %H:%M:%S'
+
+  def input
+    timestamp = @builder.object.send(attribute_name)
+    formatted = timestamp.try(:strftime, FORMAT)
+
+    options[:hint] = 1.week.from_now.strftime "Формат: #{FORMAT}"
+
+    input_html_classes.unshift 'string'
+    input_html_options[:type] = :string
+    input_html_options[:value] = formatted
+
+    @builder.text_field attribute_name, input_html_options
+  end
+end
