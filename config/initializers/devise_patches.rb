d310c8a7b8117e1b067e396e63752b1f188cf828
Dimitar Dimitrov <me@ddimitrov.name>
2013-01-29 14:14:21 +0200

    Apply the correct security patch for older Devise versions
    Info: http://blog.plataformatec.com.br/2013/01/security-announcement-devise-v2-2-3-v2-1-3-v2-0-5-and-v1-5-3-released/

--- config/initializers/devise_patches.rb
+++ config/initializers/devise_patches.rb
@@ -1,5 +1,5 @@
 # Security patch; more info here:
 # http://blog.plataformatec.com.br/2013/01/security-announcement-devise-v2-2-3-v2-1-3-v2-0-5-and-v1-5-3-released/
-Devise::ParamFilter.class_eval do
-  def param_requires_string_conversion?(_value); true; end
+Devise::Models::Authenticatable::ClassMethods.class_eval do
+  def auth_param_requires_string_conversion?(value); true; end
 end
