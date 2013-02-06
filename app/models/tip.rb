2305c7f763253261a0051337cbb206166e970344
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-06 23:41:13 +0200

    Implement Tip.default_new_pushlied_at

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,3 +1,7 @@
 class Tip < ActiveRecord::Base
-  # attr_accessible :title, :body
+  class << self
+    def default_new_pushlied_at
+      order("published_at").last.published_at + 1.day rescue Date.today
+    end
+  end
 end
