6da901d04976379667f3f790b6a6b5cbe7de6173
ignisf <bordjukov@gmail.com>
2013-02-18 00:26:42 +0200

    Update to Rails 3.2.12

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Trane::Application.config.session_store :cookie_store, :key => '_trane_session'
+Trane::Application.config.session_store :cookie_store, key: '_trane_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
