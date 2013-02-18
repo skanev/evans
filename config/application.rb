f39b025836fd16e6b8d799f304fb043242c98af4
ignisf <bordjukov@gmail.com>
2013-02-18 12:52:18 +0200

    Remove duplicate configuration entry

--- config/application.rb
+++ config/application.rb
@@ -37,9 +37,6 @@ module Trane
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Selecet the format for dumping the database schema to a file.
-    config.active_record.schema_format = :sql
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -49,7 +46,7 @@ module Trane
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
+    config.active_record.schema_format = :sql
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
