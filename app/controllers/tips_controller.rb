f4fa59344a1f202abc0b505f46d5b5cccc5905f0
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-19 13:59:04 +0200

    Implement Tip.in_reverse_chronological_order

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -3,7 +3,7 @@ class TipsController < ApplicationController
   before_filter :require_admin, except: [:index, :show]
 
   def index
-    @tips = Tip.order('published_at desc').all
+    @tips = Tip.in_reverse_chronological_order
   end
 
   def new
