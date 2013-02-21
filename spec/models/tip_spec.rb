3e285f1a7c22b9926ce933e0eaf1c5a30480d61b
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-21 15:24:04 +0200

    Fix Tip.current

--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Tip do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tip) { double }
+
+  it "suggest default value for published_at if there are no tips" do
+    Tip.default_new_pushlied_at.to_i.should eq Time.now.to_i
+  end
+
+  it "suggest default value for published_at if there are tips" do
+    first  = create :tip, published_at: 2.days.ago
+
+    Tip.default_new_pushlied_at.to_i.should eq (Time.now - 1.day).to_i
+  end
 end
