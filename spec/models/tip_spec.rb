907698de67899900aa2c79e175c4687233562954
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-21 16:24:30 +0200

    Use Timecop to test Tip.published_at

--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -4,12 +4,15 @@ describe Tip do
   let(:tip) { double }
 
   it "suggest default value for published_at if there are no tips" do
-    Tip.default_new_pushlied_at.to_i.should eq Time.now.to_i
+    Timecop.freeze do
+      Tip.default_new_pushlied_at.should be_within(1.second).of(Time.now)
+    end
   end
 
   it "suggest default value for published_at if there are tips" do
-    first  = create :tip, published_at: 2.days.ago
-
-    Tip.default_new_pushlied_at.to_i.should eq (Time.now - 1.day).to_i
+    Timecop.freeze(Time.now) do
+      first = create :tip, published_at: 2.days.ago
+      Tip.default_new_pushlied_at.should be_within(1.second).of(1.day.ago)
+    end
   end
 end
