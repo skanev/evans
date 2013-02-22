52a53354671f948f1ba0b6a2ad70858dd1b27718
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-22 18:12:13 +0200

    Re-indent a case in config/initializers/language.rb

--- config/initializers/language.rb
+++ config/initializers/language.rb
@@ -1,11 +1,11 @@
 file = Rails.root.join('config/language.txt')
 
 case Rails.env
-when "production"
-  raise 'There needs to be a config/language.txt file.' unless file.exist?
-  Language.language_name = file.read.strip
-when "test"
-  Language.language_name = 'ruby'
-else
-  Language.language_name = file.exist? ? file.read.strip : 'ruby'
+  when "production"
+    raise 'There needs to be a config/language.txt file.' unless file.exist?
+    Language.language_name = file.read.strip
+  when "test"
+    Language.language_name = 'ruby'
+  else
+    Language.language_name = file.exist? ? file.read.strip : 'ruby'
 end
