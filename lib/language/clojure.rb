87f8c593f50ca93292f22e26b7cf04d737b99482
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-22 15:51:07 +0200

    Add sample python and clojure modules in Language

---
+++ lib/language/clojure.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+module Language::Clojure
+  extend self
+
+  def language
+    :clojure
+  end
+
+  def course_name
+    "Практическо функционално програмиране с Clojure"
+  end
+
+  def run
+  end
+end
+
---
+++ lib/language/python.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+module Language::Python
+  extend self
+
+  def language
+    :python
+  end
+
+  def course_name
+    "Програмиране с Python"
+  end
+
+  def run
+  end
+end
