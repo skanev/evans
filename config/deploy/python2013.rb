fe382ae0f2a72487458538610995ac2d52336d06
Dimitar Dimitrov <me@ddimitrov.name>
2013-02-24 18:09:35 +0200

    Rename the Capistrano stage python2012 → python2013

--- config/deploy/python2013.rb
+++ config/deploy/python2013.rb
@@ -1,4 +1,4 @@
-set :deploy_to,   '/data/rails/python-2013'
+set :deploy_to,   '/data/rails/pyfmi-2013'
 role :web, 'fmi.py-bg.net'
 role :app, 'fmi.py-bg.net'
 role :db,  'fmi.py-bg.net', :primary => true
