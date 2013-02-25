0a42962152fa02df5f7c4fdc1904db24345bb99d
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-25 17:02:33 +0200

    Remove useless quotes

--- config/deploy/python_2013.rb
+++ config/deploy/python_2013.rb
@@ -6,6 +6,6 @@ role :db,  'fmi.py-bg.net', :primary => true
 
 set :default_environment, {
   'SITE_HOSTNAME' => 'fmi.py-bg.net',
-  'MAILER_FROM' => '"Python FMI" <evans@py-bg.net>',
-  'MAILER_REPLY_TO' => '"Python FMI" <fmi@py-bg.net>',
+  'MAILER_FROM' => 'Python FMI <evans@py-bg.net>',
+  'MAILER_REPLY_TO' => 'Python FMI <fmi@py-bg.net>',
 }
