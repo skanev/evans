34114fe58d30d4229dbbb54cb54f5893c54cac6e
Kiril Vladimirov <kiril@vladimiroff.org>
2013-02-25 17:57:30 +0200

    Remove ENV settings from config/deploy

--- config/deploy/python_2013.rb
+++ config/deploy/python_2013.rb
@@ -1,11 +1,4 @@
-
 set :deploy_to,   '/data/rails/pyfmi-2013'
 role :web, 'fmi.py-bg.net'
 role :app, 'fmi.py-bg.net'
 role :db,  'fmi.py-bg.net', :primary => true
-
-set :default_environment, {
-  'SITE_HOSTNAME' => 'fmi.py-bg.net',
-  'MAILER_FROM' => 'evans@py-bg.net',
-  'MAILER_REPLY_TO' => 'fmi@py-bg.net',
-}
