c7c809ebb109b3ccacb603cf5aeb52b7e3497070
Stefan Kanev <stefan.kanev@gmail.com>
2013-02-27 23:46:41 +0200

    Add missing configuration for Clojure

--- lib/language/clojure.rb
+++ lib/language/clojure.rb
@@ -10,6 +10,14 @@ module Language::Clojure
     "Практическо функционално програмиране с Clojure"
   end
 
+  def email
+    'fmi@clojure.bg'
+  end
+
+  def domain
+    'fmi.clojure.bg'
+  end
+
   def run_tests(test, solution)
     raise NotImplementedError
   end
