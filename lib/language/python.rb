f66da89efd2cc638a1f91cde275d2cf2f7d3085a
Kiril Vladimirov <kiril@vladimiroff.org>
2013-03-07 02:32:02 +0200

    Implement Language::Python.run_tests

--- lib/language/python.rb
+++ lib/language/python.rb
@@ -7,7 +7,21 @@ module Language::Python
   end
 
   def run_tests(test, solution)
-    raise NotImplementedError
+    Dir.mktmpdir do |dir|
+      test_path     = Pathname(dir).join('test.py')
+      solution_path = Pathname(dir).join('solution.py')
+
+      open(test_path, 'w') { |file| file.write test.encode('utf-8') }
+      open(solution_path, 'w') { |file| file.write solution.encode('utf-8') }
+
+      results = JSON.parse `python3.3 lib/homework/runner.py #{test_path}`
+
+      {
+        log: results['log'] || '',
+        passed: results['passed'] || 0,
+        failed: results['failed'] || 0,
+      }
+    end
   end
 end
 
