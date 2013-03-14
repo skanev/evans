4acd4bcb539ce9605e4f8acc3a2862b9c5521452
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-03-14 15:58:26 +0200

    Don't interact with the User model in the migration

--- db/migrate/20130310143220_add_token_authentication_to_user.rb
+++ db/migrate/20130310143220_add_token_authentication_to_user.rb
@@ -1,6 +1,5 @@
 class  AddTokenAuthenticationToUser < ActiveRecord::Migration
   def change
     add_column :users, :authentication_token , :string
-    User.all.each(&:reset_authentication_token)
   end
 end
