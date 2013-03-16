68ac65da487c3568f3280bf93ad4a24b119cd91d
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-03-16 19:14:55 +0200

    Faux the user model in the authentication migration

--- db/migrate/20130310143220_add_token_authentication_to_user.rb
+++ db/migrate/20130310143220_add_token_authentication_to_user.rb
@@ -1,5 +1,16 @@
-class  AddTokenAuthenticationToUser < ActiveRecord::Migration
+class AddTokenAuthenticationToUser < ActiveRecord::Migration
+  class User < ActiveRecord::Base
+    devise :token_authenticatable
+  end
+
   def change
-    add_column :users, :authentication_token , :string
+    add_column :users, :authentication_token, :string
+    ensure_users_have_authentication_tokens
+  end
+
+  private
+
+  def ensure_users_have_authentication_tokens
+    User.all.each(&:ensure_authentication_token!)
   end
 end
