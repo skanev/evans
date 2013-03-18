a58578dd3f4d64443f91d19c456b48f99ad8ad5d
Stefan Kanev <stefan.kanev@gmail.com>
2013-03-18 16:56:38 +0200

    Add an intermediary TestRunner::Results class
    The main goal is to remove the TestRunner class and replace it
    with code in Language.run_test

--- lib/language/ruby.rb
+++ lib/language/ruby.rb
@@ -7,22 +7,19 @@ module Language::Ruby
   end
 
   def run_tests(test, solution)
-    Dir.mktmpdir do |dir|
-      spec_path     = Pathname(dir).join('spec.rb')
-      solution_path = Pathname(dir).join('solution.rb')
+    TestRunner.with_tmpdir('spec.rb' => test, 'solution.rb' => solution) do |dir|
+      spec_path     = dir.join('spec.rb')
+      solution_path = dir.join('solution.rb')
 
-      open(spec_path, 'w') { |file| file.write test.encode('utf-8') }
-      open(solution_path, 'w') { |file| file.write solution }
-
-      output = `ruby lib/homework/runner.rb #{spec_path} #{solution_path}`
+      output = `ruby lib/language/ruby/runner.rb #{spec_path} #{solution_path}`
       json, log = output.split("\nLOG:\n", 2)
       results = JSON.parse json
 
-      {
+      TestRunner::Results.new({
         log: log,
         passed: results['passed'] || [],
         failed: results['failed'] || [],
-      }
+      })
     end
   end
 end
--- lib/test_runner.rb
+++ lib/test_runner.rb
@@ -1,6 +1,24 @@
 class TestRunner
   attr_reader :passed_count, :failures_count, :log
 
+  class Results
+    attr_accessor :passed, :failed, :log
+
+    def initialize(attributes = {})
+      attributes.each do |key, value|
+        send "#{key}=", value
+      end
+    end
+
+    def passed_count
+      passed.count
+    end
+
+    def failed_count
+      failed.count
+    end
+  end
+
   def initialize(test, solution)
     @test     = test
     @solution = solution
@@ -11,19 +29,32 @@ class TestRunner
       runner = new(test, solution)
       runner.run
 
-      {
+      TestResults.new({
         passed: runner.passed_count,
         failed: runner.failures_count,
         log: runner.log,
-      }
+      })
+    end
+
+    def with_tmpdir(files)
+      Dir.mktmpdir do |dir|
+        dir_path = Pathname(dir)
+
+        files.each do |name, contents|
+          file_path = dir_path.join(name)
+          open(file_path, 'w') { |file| file.write contents.encode('utf-8') }
+        end
+
+        yield dir_path
+      end
     end
   end
 
   def run
     result = Language.run_tests(@test, @solution)
 
-    @log            = result[:log]
-    @passed_count   = result[:passed].count
-    @failures_count = result[:failed].count
+    @log            = result.log
+    @passed_count   = result.passed_count
+    @failures_count = result.failed_count
   end
 end
