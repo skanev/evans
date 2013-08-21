6be5d1f282c8930d126c74137bc4d0a03121b146
Hristo Vladev <hr.vladev@gmail.com>
2013-08-22 00:51:43 +0300

    Fixed: changing password logs you out.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,9 +9,16 @@ class ProfilesController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes params[:user]
+      sign_in @user, bypass: true if password_updated?
       redirect_to dashboard_path, notice: 'Профилът ви е обновен'
     else
       render :edit
     end
+  end
+
+  private
+
+  def password_updated?
+    !params[:user][:password].blank?
   end
 end
