eef9eca9e2c42ce98ff02949955d5eab4496c3f2
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-08 17:29:33 +0300

    Add a basic language definition for Go

---
+++ lib/language/go.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+module Language::Go
+  extend self
+
+  def language
+    :go
+  end
+
+  def extension
+    'go'
+  end
+
+  def solution_dump(attributes)
+    <<-END
+// #{attributes[:name]}
+// #{attributes[:faculty_number]}
+// #{attributes[:url]}
+
+#{attributes[:code]}
+
+// Log output
+// ----------
+#{attributes[:log].lines.map { |line| "// #{line}".strip }.join("\n")}
+    END
+  end
+
+  def run_tests(test, solution)
+    raise 'Not implemented'
+  end
+end
