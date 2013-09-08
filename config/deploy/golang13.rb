6375b5e1372358969811fd45a1fcee7dc7445fb0
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-08 17:17:07 +0300

    Add a Capistrano stage config for Go

---
+++ config/deploy/golang13.rb
@@ -0,0 +1,7 @@
+set :deploy_to, '/data/rails/golang-2013'
+set :database_name, 'golang_2013'
+
+role :web,       '2013.fmi.golang.bg'
+role :app,       '2013.fmi.golang.bg'
+role :db,        '2013.fmi.golang.bg', primary: true
+role :sidekiq,   '2013.fmi.golang.bg'
