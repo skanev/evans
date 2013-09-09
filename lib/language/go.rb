80b3251f420d6c3bc34aff70352bdd614aee5fe5
Andrew Radev <andrey.radev@gmail.com>
2013-09-09 22:13:23 +0200

    Working go tests

--- lib/language/go.rb
+++ lib/language/go.rb
@@ -24,6 +24,16 @@ module Language::Go
   end
 
   def run_tests(test, solution)
-    raise 'Not implemented'
+    TestRunner.with_tmpdir('solution_test.go' => test, 'solution.go' => solution) do |dir|
+      test_path = dir.join('solution_test.go')
+
+      results = JSON.parse(`go run lib/language/go/runner.go -- #{test_path}`.strip)
+
+      TestRunner::Results.new({
+        log:    results['Log'] || '',
+        passed: results['Passed'] || [],
+        failed: results['Failed'] || [],
+      })
+    end
   end
 end
