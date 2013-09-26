348106f8c5b028647b150fc6ee6688e429b6b5dc
Stefan Kanev <stefan.kanev@gmail.com>
2013-09-26 12:27:58 +0300

    Clean up attribution_spec a bit

--- spec/models/attribution_spec.rb
+++ spec/models/attribution_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe Attribution do
-  it 'requires proper link format' do
-    attribution = Attribution.new link: 'random'
-    attribution.should have_error_on :link
-  end
-
-  it 'accepts proper link format' do
-    attribution = Attribution.new link: 'http://example.com'
-    attribution.should_not have_error_on :link
-  end
+  it { should allow_value('http://example.com/').for(:link) }
+  it { should_not allow_value('non-url').for(:link) }
 end
