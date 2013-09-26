c79b4d76169154dac3b46ac05ae3f4abb38a1fc8
Stefan Kanev <stefan.kanev@gmail.com>
2013-09-26 12:28:20 +0300

    Simplify the validations of Attributions

--- app/models/attribution.rb
+++ app/models/attribution.rb
@@ -5,13 +5,5 @@ class Attribution < ActiveRecord::Base
   attr_accessible :reason, :link
 
   validates :reason, :link, presence: true
-  validate :ensure_proper_link_format
-
-  private
-
-  def ensure_proper_link_format
-    unless link =~ URI.regexp
-      errors.add :link, 'Това не е истински URL'
-    end
-  end
+  validates :link, format: URI.regexp
 end
