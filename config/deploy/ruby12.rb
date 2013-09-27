1463fecb158dd8f3d4ce0cd029423ce0bd12053d
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 20:56:44 +0300

    Use domain + year in the ruby12 deploy config
    This style allows us to avoid changes in the deploy config once the
    course is superseded by a new one.

--- config/deploy/ruby12.rb
+++ config/deploy/ruby12.rb
@@ -1,6 +1,7 @@
-set :deploy_to,   '/data/rails/evans-2012'
-role :web, 'fmi.ruby.bg'
-role :app, 'fmi.ruby.bg'
-role :db,  'fmi.ruby.bg', :primary => true
-
+set :deploy_to, '/data/rails/evans-2012'
 set :database_name, 'evans_2012'
+
+role :web,       '2012.fmi.ruby.bg'
+role :app,       '2012.fmi.ruby.bg'
+role :db,        '2012.fmi.ruby.bg', primary: true
+role :sidekiq,   '2012.fmi.ruby.bg'
