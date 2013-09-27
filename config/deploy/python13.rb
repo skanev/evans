4983e8ce0c0e4d0bbcb949a9d06034454bb74687
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 21:52:57 +0300

    Update the python13 deployment stage
    It should use full domain names, including the course's year.

--- config/deploy/python13.rb
+++ config/deploy/python13.rb
@@ -1,7 +1,7 @@
-set :deploy_to,   '/data/rails/pyfmi-2013'
-role :web,     'fmi.py-bg.net'
-role :app,     'fmi.py-bg.net'
-role :db,      'fmi.py-bg.net', :primary => true
-role :sidekiq, 'fmi.py-bg.net'
-
+set :deploy_to, '/data/rails/pyfmi-2013'
 set :database_name, 'pyfmi_2013'
+
+role :web,     '2013.fmi.py-bg.net'
+role :app,     '2013.fmi.py-bg.net'
+role :db,      '2013.fmi.py-bg.net', primary: true
+role :sidekiq, '2013.fmi.py-bg.net'
