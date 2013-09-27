692262a16dbc3d970e37796c76db6a174dacd85c
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 22:32:33 +0300

    Minor code alignment fix

--- config/deploy/ruby12.rb
+++ config/deploy/ruby12.rb
@@ -1,7 +1,7 @@
 set :deploy_to, '/data/rails/evans-2012'
 set :database_name, 'evans_2012'
 
-role :web,       '2012.fmi.ruby.bg'
-role :app,       '2012.fmi.ruby.bg'
-role :db,        '2012.fmi.ruby.bg', primary: true
-role :sidekiq,   '2012.fmi.ruby.bg'
+role :web,     '2012.fmi.ruby.bg'
+role :app,     '2012.fmi.ruby.bg'
+role :db,      '2012.fmi.ruby.bg', primary: true
+role :sidekiq, '2012.fmi.ruby.bg'
