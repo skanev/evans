e73bbe590c15ca7bd3e9028e637a81be5a9ab10a
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 21:02:21 +0300

    Remove a duplicated bundle install during deployment
    It is handled by the default Capistrano Bundler tasks included in
    Capfile by bundler/capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,11 +16,6 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 
-  task :setup_gems, :except => {:no_release => true} do
-    run "ln -nfs #{shared_path}/bundle #{release_path}/vendor/bundle"
-    run "cd '#{release_path}' && bundle install --deployment"
-  end
-
   task :symlink_shared, :roles => :app do
     run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/site.yml     #{release_path}/config/site.yml"
@@ -41,7 +36,6 @@ namespace :deploy do
 end
 
 after 'deploy:setup',       'deploy:setup_shared'
-after 'deploy:update_code', 'deploy:setup_gems'
 after 'deploy:update_code', 'deploy:symlink_shared'
 after 'deploy:update_code', 'deploy:assets:precompile'
 
