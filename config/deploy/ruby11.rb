685ba06f0e0ff0c43b0b6aa03ab5d60b5c668107
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 22:32:43 +0300

    Add a deploy config for ruby 2011

---
+++ config/deploy/ruby11.rb
@@ -0,0 +1,7 @@
+set :deploy_to, '/data/rails/evans-2011'
+set :database_name, 'evans_2011'
+
+role :web,     '2011.fmi.ruby.bg'
+role :app,     '2011.fmi.ruby.bg'
+role :db,      '2011.fmi.ruby.bg', primary: true
+role :sidekiq, '2011.fmi.ruby.bg'
