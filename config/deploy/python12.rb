ac56f5f377a94a36dcf12095529fa3f24af7afdd
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 21:57:51 +0300

    Add a deployment stage for python-2012

---
+++ config/deploy/python12.rb
@@ -0,0 +1,7 @@
+set :deploy_to, '/data/rails/pyfmi-2012'
+set :database_name, 'pyfmi_2012'
+
+role :web,     '2012.fmi.py-bg.net'
+role :app,     '2012.fmi.py-bg.net'
+role :db,      '2012.fmi.py-bg.net', primary: true
+role :sidekiq, '2012.fmi.py-bg.net'
