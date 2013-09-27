322eebc6ba64cd6906ae3979ab1fc474a9500939
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-27 23:36:38 +0300

    Add a deployment config for Ruby 2013

---
+++ config/deploy/ruby13.rb
@@ -0,0 +1,7 @@
+set :deploy_to, '/data/rails/evans-2013'
+set :database_name, 'evans_2013'
+
+role :web,     '2013.fmi.ruby.bg'
+role :app,     '2013.fmi.ruby.bg'
+role :db,      '2013.fmi.ruby.bg', primary: true
+role :sidekiq, '2013.fmi.ruby.bg'
