df99218233403c54b1b0a6b0157be8469e2c6c81
Stefan Kanev <stefan.kanev@gmail.com>
2013-09-28 20:17:34 +0300

    Show the next fake number in sign_ups#index
    Fixes #54

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,5 +41,18 @@ class User < ActiveRecord::Base
       END
       order(sort_order).paginate page: page_number, per_page: 32
     end
+
+    def next_fake_faculty_number
+      last_number = User.
+        where("faculty_number LIKE 'x%'").
+        pluck(:faculty_number).
+        map { |n| n[/^x(\d+)$/, 1] }.
+        compact.
+        map(&:to_i).
+        sort.
+        last || 0
+
+      sprintf 'x%05d', last_number + 1
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,4 +43,17 @@ describe User do
       User.page(1).should eq [first, second]
     end
   end
+
+  describe "generating fake faculty numbers" do
+    it "returns 'x00001' if no fakes in the database" do
+      User.next_fake_faculty_number.should eq 'x00001'
+    end
+
+    it "increments the largest fake faculty number if one exists" do
+      create :user, faculty_number: 'x00041'
+      create :user, faculty_number: 'x00004'
+
+      User.next_fake_faculty_number.should eq 'x00042'
+    end
+  end
 end
