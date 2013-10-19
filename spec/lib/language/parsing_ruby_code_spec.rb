f4c2adc7d7f8bda1f1d204310a90868391d68fb4
Stanislav Gatev <s2gatev@gmail.com>
2013-10-19 21:03:11 +0300

    Refactor spec for parsing Ruby code

--- spec/lib/language/parsing_ruby_code_spec.rb
+++ spec/lib/language/parsing_ruby_code_spec.rb
@@ -1,31 +1,24 @@
 require 'spec_helper'
 
 describe "Parsing Ruby code", ruby: true do
-  before :all do
-    @invalid_code = <<END.strip
+  it "returns false for invalid code" do
+    invalid_code = <<END.strip
 def answer()
   42
 END
+    Language::Ruby.parses?(invalid_code).should be_false
+  end
 
-    @valid_code = <<END.strip
+  it "returns true for valid code" do
+    valid_code = <<END.strip
 def answer()
   42
 end
 END
-
-    @empty_code = <<END.strip
-END
-  end
-
-  it "returns false for invalid code" do
-    Language::Ruby.parses?(@invalid_code).should be_false
-  end
-
-  it "returns true for valid code" do
-    Language::Ruby.parses?(@valid_code).should be_true
+    Language::Ruby.parses?(valid_code).should be_true
   end
 
   it "returns true for no code" do
-    Language::Ruby.parses?(@empty_code).should be_true
+    Language::Ruby.parses?("").should be_true
   end
 end
