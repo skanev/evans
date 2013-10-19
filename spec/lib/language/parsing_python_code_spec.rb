a8ed79d91d1ff4079efe9b98b0ccdcdbd6fba498
Stanislav Gatev <s2gatev@gmail.com>
2013-10-19 23:28:51 +0300

    Refactor specs for Language#parses? method

--- spec/lib/language/parsing_python_code_spec.rb
+++ spec/lib/language/parsing_python_code_spec.rb
@@ -2,19 +2,17 @@ require 'spec_helper'
 
 describe "Parsing Python code", python: true do
   it "returns false for invalid code" do
-    invalid_code = <<END.strip
+    Language::Python.parses?(<<-END).should be_false
 def class answer:
   42
 END
-    Language::Python.parses?(invalid_code).should be_false
   end
 
   it "returns true for valid code" do
-    valid_code = <<END.strip
+    Language::Python.parses?(<<-END).should be_true
 def answer():
   return 42
 END
-    Language::Python.parses?(valid_code).should be_true
   end
 
   it "returns true for no code" do
--- spec/lib/language/parsing_ruby_code_spec.rb
+++ spec/lib/language/parsing_ruby_code_spec.rb
@@ -2,20 +2,18 @@ require 'spec_helper'
 
 describe "Parsing Ruby code", ruby: true do
   it "returns false for invalid code" do
-    invalid_code = <<END.strip
+    Language::Ruby.parses?(<<-END).should be_false
 def answer()
   42
 END
-    Language::Ruby.parses?(invalid_code).should be_false
   end
 
   it "returns true for valid code" do
-    valid_code = <<END.strip
+    Language::Ruby.parses?(<<-END).should be_true
 def answer()
   42
 end
 END
-    Language::Ruby.parses?(valid_code).should be_true
   end
 
   it "returns true for no code" do
