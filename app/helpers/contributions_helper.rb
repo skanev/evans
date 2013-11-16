4a23afa9f5db4a139cde2a8b63f9c40b6d5c90fe
Stanislav Gatev <s2gatev@gmail.com>
2013-11-17 00:07:34 +0200

    Remove unneeded changes in ContributionHelper

--- app/helpers/contributions_helper.rb
+++ app/helpers/contributions_helper.rb
@@ -1,12 +1,10 @@
 module ContributionsHelper
-  def decorate_contribution(model)
-    case model
+  def contribution(model)
+    contribution = case model
       when Comment then CommentDecorator.decorate model
       when Post    then PostDecorator.decorate model
     end
-  end
 
-  def contribution(model)
-    render 'common/contribution', contribution: decorate_contribution(model)
+    render 'common/contribution', contribution: contribution
   end
 end
