c37d13d7b550ab5a4c88c6f2943c6e96917ec1a3
Stanislav Gatev <s2gatev@gmail.com>
2013-11-20 10:40:45 +0200

    Minor refactoring in MarkdownInput

--- app/inputs/markdown_input.rb
+++ app/inputs/markdown_input.rb
@@ -3,7 +3,9 @@ class MarkdownInput < SimpleForm::Inputs::TextInput
     content = @builder.object.send(attribute_name) || ''
     input_html_options['data-contribution-input'] = true
 
-    super + template.content_tag(:div, Markup.format(content),
+    preview_area = template.content_tag(:div, Markup.format(content),
       data: {'contribution-preview' => 'true'}, class: 'contribution-preview')
+
+    super + preview_area
   end
 end
