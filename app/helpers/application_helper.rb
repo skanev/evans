48535171b937e273d97ae51ed68b16323c04a11e
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-11-30 18:06:38 +0200

    Remove the title out of the image tags
    I spoke with @skanev about it and he doesn't like it.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
 module ApplicationHelper
   def user_thumbnail(user, version = :size150)
-    title = "Екип" if user.admin?
     image = user.photo.try(:url, version) || image_path("photoless-user/#{version}.png")
-
     css_classes = %w(avatar)
     css_classes << "admin" if user.admin?
 
-    image_tag image, alt: user.name, class: css_classes, title: title
+    image_tag image, alt: user.name, class: css_classes
   end
 
   def markup(text, options = {})
