6f3614f112fe6771baa5eff2fee933d9a4fa78ab
Stanislav Gatev <s2gatev@gmail.com>
2013-12-21 19:51:56 +0200

    Add tasks and challenges to user dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,32 @@
 class DashboardsController < ApplicationController
+  include ChallengesHelper
+
   before_filter :require_user
 
   def show
     @points = current_user.points
     @rank   = current_user.rank
     @total  = PointsBreakdown.count
+
+    task_names = Task.all.map &:name
+    task_points = PointsBreakdown.find(current_user.id).tasks_breakdown
+    @tasks = task_names.zip task_points
+
+    challenge_solutions = Hash[
+      ChallengeSolution.where(user: current_user).map do |solution|
+        [solution.challenge.name, solution]
+      end
+    ]
+    @challenges = Challenge.visible.map do |challenge|
+      solution_status = case
+                        when challenge_solutions.has_key?(challenge.name)
+                          challenge_solution_status challenge_solutions[challenge.name]
+                        when challenge.checked?
+                          :incorrect
+                        else
+                          :unchecked
+                        end
+      [challenge.name, solution_status]
+    end
   end
 end
--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -7,7 +7,12 @@ describe DashboardsController do
     before do
       current_user.stub :points
       current_user.stub :rank
+      current_user.stub id: 42
       PointsBreakdown.stub :count
+      points_breakdown = PointsBreakdown.new
+      points_breakdown.stub tasks_breakdown: []
+      PointsBreakdown.stub find: points_breakdown
+      ChallengeSolution.stub where: []
     end
 
     it "requires an authenticated user" do
@@ -20,6 +25,26 @@ describe DashboardsController do
       current_user.stub points: 2
       get :show
       assigns(:points).should eq 2
+    end
+
+    it "assigns the tasks and points of the current user for particular task to @tasks" do
+      task = Task.new name: "Some task"
+      Task.should_receive(:all).and_return [task]
+      points_breakdown = PointsBreakdown.new
+      points_breakdown.stub tasks_breakdown: [0]
+      PointsBreakdown.stub find: points_breakdown
+
+      get :show
+      assigns(:tasks).should eq [[task.name, 0]]
+    end
+
+    it "assigns the challenges and points of the current user for particular challenge to @challenges" do
+      challenge = Challenge.new name: "Some challenge", checked: false, hidden: false
+      Challenge.should_receive(:visible).and_return [challenge]
+      challenge_solution = ChallengeSolution.new challenge: challenge
+      ChallengeSolution.stub where: [challenge_solution]
+      get :show
+      assigns(:challenges).should eq [[challenge.name, :unchecked]]
     end
   end
 end
