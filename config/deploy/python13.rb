0ec2b1c20cecc633e82240960fd2745e54dd24fb
Dimitar Dimitrov <me@ddimitrov.name>
2014-02-04 18:36:44 +0200

    Don't start sidekiq for Python 2013

--- config/deploy/python13.rb
+++ config/deploy/python13.rb
@@ -4,4 +4,3 @@ set :database_name, 'pyfmi_2013'
 role :web,     '2013.fmi.py-bg.net'
 role :app,     '2013.fmi.py-bg.net'
 role :db,      '2013.fmi.py-bg.net', primary: true
-role :sidekiq, '2013.fmi.py-bg.net'
