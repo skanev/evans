0c3644139fc1e508bc4a9021277bb55ce4f0c356
Vassil Kalkov <vassilkalkov@gmail.com>
2014-04-06 16:12:32 +0300

    Fix bulgarian translation of word voucher

--- features/step_definitions/voucher_steps.rb
+++ features/step_definitions/voucher_steps.rb
@@ -1,20 +1,20 @@
-Дадено 'че съществува свободен ваучър "$code"' do |code|
+Дадено 'че съществува свободен ваучер "$code"' do |code|
   create :voucher, code: code
 end
 
-Когато 'добавя следните ваучъри:' do |codes|
+Когато 'добавя следните ваучери:' do |codes|
   visit new_voucher_path
   fill_in 'Кодове', with: codes
   click_on 'Създай'
 end
 
-Когато 'въведа ваучър "$code"' do |code|
+Когато 'въведа ваучер "$code"' do |code|
   visit new_voucher_claim_path
   fill_in 'Код', with: code
   click_on 'Въведи'
 end
 
-То 'трябва да съществуват следните ваучъри:' do |table|
+То 'трябва да съществуват следните ваучери:' do |table|
   visit vouchers_path
 
   codes = all('table tr td:first-child').map { |element| [element.text] }
@@ -22,6 +22,6 @@ end
   table.diff! codes
 end
 
-То /^трябва да имам "(\d+)" точк(?:а|и) от ваучъри$/ do |count|
+То /^трябва да имам "(\d+)" точк(?:а|и) от ваучери$/ do |count|
   Voucher.where(user_id: @current_user.id).count.should eq count.to_i
 end
