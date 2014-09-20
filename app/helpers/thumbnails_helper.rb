b74c0fc225046119232563c1562b8a1127e92385
Stanislav Gatev <s2gatev@gmail.com>
2014-09-20 20:58:41 +0300

    Extract thumbail creation in leaderboards to helper

--- app/helpers/thumbnails_helper.rb
+++ app/helpers/thumbnails_helper.rb
@@ -1,4 +1,17 @@
 module ThumbnailsHelper
+  def breakdown_thumbnail(breakdown, version = :size30)
+    image = if breakdown.photo.zero?
+              "photoless-user/#{version}.png"
+            else
+              "/uploads/photos/#{breakdown.id}/#{version}_photo.jpg"
+            end
+
+    css_styles = []
+    css_styles += grayscale_filters_for(breakdown.total) if grayscale_user_thumbnails?
+
+    image_tag image, alt: breakdown.name, style: css_styles.join
+  end
+
   def user_thumbnail(user, version = :size150)
     image = user.photo.try(:url, version) || image_path("photoless-user/#{version}.png")
 
@@ -6,7 +19,7 @@ module ThumbnailsHelper
     css_classes << "admin" if user.admin?
 
     css_styles = []
-    css_styles += grayscale_filters_for(user) if grayscale_user_thumbnails? and not user.admin?
+    css_styles += grayscale_filters_for(user.points) if grayscale_user_thumbnails? and not user.admin?
 
     image_tag image, alt: user.name, class: css_classes, style: css_styles.join
   end
@@ -21,8 +34,8 @@ module ThumbnailsHelper
     Rails.application.config.try(:user_target_points, 100)
   end
 
-  def grayscale_filters_for(user)
-    user_progress = user.points.to_f / user_target_points * 100
+  def grayscale_filters_for(points)
+    user_progress = points.to_f / user_target_points * 100
     grayscale_level = [100 - user_progress, 0].max
 
     ["-webkit-filter", "-moz-filter", "filter"].map do |filter_type|
