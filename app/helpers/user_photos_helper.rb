36185ae391f4bb307dc4451af692002674d0f310
Stanislav Gatev <s2gatev@gmail.com>
2014-09-24 14:16:32 +0300

    Make admin photos fully-colored

--- app/helpers/user_photos_helper.rb
+++ app/helpers/user_photos_helper.rb
@@ -6,20 +6,20 @@ module UserPhotosHelper
                    "/uploads/photos/#{breakdown.id}/#{version}_photo.jpg"
                  end
 
-
     UserPhotoGenerator.new.generate_photo image_path: image_path,
+                                          grayscale_photo: true,
                                           user_points: breakdown.total
   end
 
   def user_photo_for_avatar(user, version = :size150)
     image_path = user.photo.try(:url, version) || image_path("photoless-user/#{version}.png")
-    user_points = user.admin? ? 0 : user.points
 
     css_classes = %w(avatar)
     css_classes << "admin" if user.admin?
 
     UserPhotoGenerator.new.generate_photo image_path: image_path,
-                                          user_points: user_points,
+                                          grayscale_photo: !user.admin?,
+                                          user_points: user.points,
                                           css_classes: css_classes
   end
 end
--- app/models/user_photo_generator.rb
+++ app/models/user_photo_generator.rb
@@ -1,8 +1,8 @@
 class UserPhotoGenerator
   include ActionView::Helpers::AssetTagHelper
 
-  def generate_photo(image_path: image_path, user_points: points, css_classes: [], css_styles: [])
-    css_styles += css_filters_for_points(user_points)
+  def generate_photo(image_path: image_path, user_points: points, grayscale_photo: false, css_classes: [], css_styles: [])
+    css_styles += css_filters_for_points(user_points) if grayscale_user_thumbnails? and grayscale_photo
 
     image_tag image_path, class: css_classes, style: css_styles.join
   end
@@ -10,8 +10,6 @@ class UserPhotoGenerator
   private
 
   def css_filters_for_points(points)
-    return [] unless grayscale_user_thumbnails?
-
     user_progress = points.to_f / points_required_for_fully_colored_image * 100
     grayscale_level = [100 - user_progress, 0].max
 
@@ -25,6 +23,6 @@ class UserPhotoGenerator
   end
 
   def points_required_for_fully_colored_image
-    Rails.application.config.try(:user_target_points, 100)
+    Rails.application.config.try :user_target_points
   end
 end
