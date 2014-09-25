fdbcdeafc697f38259909381a1a08e479e64855c
Georgy Angelov <georgyangelov@gmail.com>
2014-09-25 09:23:37 +0300

    Small style and notice text change

--- app/controllers/email_notifications_controller.rb
+++ app/controllers/email_notifications_controller.rb
@@ -3,15 +3,15 @@ class EmailNotificationsController < ApplicationController
 
   def new_challenge
     challenge = Challenge.find params[:id]
-    ChallengeMailer.new_challenge challenge unless challenge.hidden
+    ChallengeMailer.new_challenge challenge unless challenge.hidden?
 
-    redirect_to challenge_path(challenge), notice: 'Мейлите са поставени в опашката'
+    redirect_to challenge_path(challenge), notice: 'Засилихме писмата'
   end
 
   def new_task
     task = Task.find params[:id]
-    TaskMailer.new_task task unless task.hidden
+    TaskMailer.new_task task unless task.hidden?
 
-    redirect_to task_path(task), notice: 'Мейлите са поставени в опашката'
+    redirect_to task_path(task), notice: 'Засилихме писмата'
   end
 end
