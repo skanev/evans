259bd040a8747db361fc22ce246c4101e28373b4
Georgy Angelov <georgyangelov@gmail.com>
2014-09-27 11:03:25 +0300

    Move some tests out of the shared ones for the mailers
    The shared examples in their previous form could not be used for
    any other mailer, even though there may be other mailers that send
    emails to multiple users. Also, the parametrization of the shared
    examples makes it harder to follow the expectations of the tests.
    
    Now, only the examples for the `#new_challenge` and `#new_task` methods
    are shared. This will make the shared test usable for other mailers.
    The other option is to make another mailer whose job is to retrieve
    all users from the database and call another mailer, but the method
    itself is small and making it more generic will be almost like
    recreating the functionality of `User.where.each`.
    
    The expectations in the separate mailer spec files are similar, but
    for other similar mailers they may be different. For example an
    announcement mailer would not contain the title of the announcement
    in the body text (only in the subject) and would not use
    `announcement_url(announcement)` to generate its URL.

--- spec/support/shared_examples/user_notificaiton_mailer_examples.rb
+++ spec/support/shared_examples/user_notificaiton_mailer_examples.rb
@@ -1,56 +1,27 @@
-shared_examples_for 'a user notification mailer' do |model, mailer, subject|
-  let(:model_instance) { double model }
+shared_examples_for 'a user notification mailer' do |mailer, method, email_method, user_filter|
+  let(:mailer_delay)   { double "#{mailer}.delay" }
+  let(:model_instance) { double 'model' }
 
-  describe "#new_#{model}" do
-    let(:mailer_delay) { double "#{mailer}.delay" }
-
-    before do
-      mailer.stub(:delay) { mailer_delay }
-    end
-
-    it 'sends multiple emails' do
-      first_user  = create :user, "#{model}_notification" => true
-      second_user = create :user, "#{model}_notification" => true
-
-      mailer_delay.should_receive("new_#{model}_for_user").with(model_instance, first_user)
-      mailer_delay.should_receive("new_#{model}_for_user").with(model_instance, second_user)
-
-      mailer.public_send "new_#{model}", model_instance
-    end
-
-    it 'does not send emails to unsubscribed users' do
-      create :user, "#{model}_notification" => false
-      user = create :user, "#{model}_notification" => true
-
-      mailer_delay.should_receive("new_#{model}_for_user").with(model_instance, user)
-
-      mailer.public_send "new_#{model}", model_instance
-    end
+  before do
+    mailer.stub(:delay) { mailer_delay }
   end
 
-  describe "#new_#{model}_for_user" do
-    subject { mailer.public_send "new_#{model}_for_user", model_instance, user }
+  it 'sends multiple emails' do
+    first_user  = create :user, user_filter => true
+    second_user = create :user, user_filter => true
 
-    let(:user) { double 'user' }
-    let(:time) { Time.now }
+    mailer_delay.should_receive(email_method).with(model_instance, first_user)
+    mailer_delay.should_receive(email_method).with(model_instance, second_user)
 
-    before do
-      user.stub({
-        first_name: 'John',
-        email: 'john@doe.com'
-      })
+    mailer.public_send method, model_instance
+  end
 
-      model_instance.stub({
-        name: 'Name',
-        closes_at: time
-      })
-    end
+  it 'does not send emails to unsubscribed users' do
+    create :user, user_filter => false
+    user = create :user, user_filter => true
 
-    it { should have_subject   subject % 'Name' }
-    it { should deliver_to     'john@doe.com' }
-    it { should have_body_text 'John' }
-    it { should have_body_text 'Name' }
-    it { should have_body_text public_send "#{model}_url", model_instance }
-    it { should have_body_text I18n.l(time) }
+    mailer_delay.should_receive(email_method).with(model_instance, user)
+
+    mailer.public_send method, model_instance
   end
 end
