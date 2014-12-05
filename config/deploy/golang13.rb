02d6b6ad56df7b9a78d7a3e85c8799e082bd27cc
Dimitar Dimitrov <me@ddimitrov.name>
2014-12-05 12:23:04 +0200

    Don't start a sidekiq daemon for these old site courses

--- config/deploy/golang13.rb
+++ config/deploy/golang13.rb
@@ -4,4 +4,3 @@ set :database_name, 'golang_2013'
 role :web,       '2013.fmi.golang.bg'
 role :app,       '2013.fmi.golang.bg'
 role :db,        '2013.fmi.golang.bg', primary: true
-role :sidekiq,   '2013.fmi.golang.bg'
--- config/deploy/ruby13.rb
+++ config/deploy/ruby13.rb
@@ -4,4 +4,3 @@ set :database_name, 'evans_2013'
 role :web,     '2013.fmi.ruby.bg'
 role :app,     '2013.fmi.ruby.bg'
 role :db,      '2013.fmi.ruby.bg', primary: true
-role :sidekiq, '2013.fmi.ruby.bg'
