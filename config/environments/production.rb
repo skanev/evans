a92b655ea76f578837a2a215fae46f298690a726
Andrew Radev <andrey.radev@gmail.com>
2019-11-16 13:15:23 +0200

    Rails 4.2 defaults for prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Trane::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -34,8 +34,8 @@ Trane::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+  # See everything in the log
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
