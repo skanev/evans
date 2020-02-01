8b52ee4ebc7b96809286ad41c3213e2e89a6a5fd
Andrew Radev <andrey.radev@gmail.com>
2020-02-01 13:04:22 +0200

    Fix spacing

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -9,12 +9,12 @@ describe Feed do
 
     item = last_activity
 
-    expect(item.user_id).to          eq commenter.id
-    expect(item.user_name).to        eq commenter.name
-    expect(item.target_id).to        eq solution.id
-    expect(item.secondary_id).to     eq task.id
-    expect(item.subject).to          eq task.name
-    expect(item.kind).to             eq :comment
+    expect(item.user_id).to      eq commenter.id
+    expect(item.user_name).to    eq commenter.name
+    expect(item.target_id).to    eq solution.id
+    expect(item.secondary_id).to eq task.id
+    expect(item.subject).to      eq task.name
+    expect(item.kind).to         eq :comment
 
     expect(item.happened_at.to_s(:db)).to eq comment.created_at.to_s(:db)
   end
@@ -26,12 +26,12 @@ describe Feed do
 
     item = last_activity
 
-    expect(item.user_id).to          eq user.id
-    expect(item.user_name).to        eq user.name
-    expect(item.target_id).to        eq solution.id
-    expect(item.secondary_id).to     eq task.id
-    expect(item.subject).to          eq task.name
-    expect(item.kind).to             eq :solution
+    expect(item.user_id).to      eq user.id
+    expect(item.user_name).to    eq user.name
+    expect(item.target_id).to    eq solution.id
+    expect(item.secondary_id).to eq task.id
+    expect(item.subject).to      eq task.name
+    expect(item.kind).to         eq :solution
 
     expect(item.happened_at.to_s(:db)).to eq solution.updated_at.to_s(:db)
   end
