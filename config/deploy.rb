f85eb6cb2d314b2c5d93898c8f1d3185ede2885c
Joan Karadimov <joan.karadimov@gmail.com>
2022-11-11 10:19:49 +0200

    Set joankaradimov/evans as repo to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ require 'capistrano/ext/multistage'
 
 set :application,   'Trane Revisited'
 set :scm,           :git
-set :repository,    'git://github.com/skanev/evans.git'
+set :repository,    'https://github.com/joankaradimov/evans.git'
 set :branch,        'master'
 set :user,          'pyfmi'
 set :user_and_host, 'pyfmi@deedee.hno3.org'
